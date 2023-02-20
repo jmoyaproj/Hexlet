b5d61f66c0d0cc556ec11751ce8fccd52831b0c3
jmoyaproj <jmoyaproj@gmail.com>
2023-02-19 22:15:24 -0600

    Completed Arithmetic Operations.

---
+++ Exercises/03-Arithmetic-Operations.js
@@ -0,0 +1,11 @@
+// Write a program that calculates and prints sequentially (one value in each line)
+// the values of the following mathematical expressions:
+//      - 3 to the power of 5
+//      - -8 divided by -4
+//      - the remainder of dividing 100 by 3
+//      - the sum of the three previous expressions
+
+console.log(3 ** 5);
+console.log(-8 / -4);
+console.log(100 % 3);
+console.log((3 ** 5) + (-8 / -4) + (100 % 3));
