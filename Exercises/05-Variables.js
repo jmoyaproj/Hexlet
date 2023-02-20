4b92606431a77b0641daa21351fbe14cbc71f670
jmoyaproj <jmoyaproj@gmail.com>
2023-02-20 10:32:32 -0600

    Completed Variables.

---
+++ Exercises/05-Variables.js
@@ -0,0 +1,8 @@
+// Find an undeclared variable in the code and declare it by assigning the value Dragon.
+
+let family = 'Targaryen';
+let pet = 'Dragon';        // The undeclared variable.
+
+console.log(family);
+console.log('and');
+console.log(pet);
