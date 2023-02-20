85fb122dfab83f935efc863311c4faeb04ca1822
jmoyaproj <jmoyaproj@gmail.com>
2023-02-20 10:23:24 -0600

    Completed Strings.

---
+++ Exercises/04-Strings.js
@@ -0,0 +1,6 @@
+// Write a program that prints:
+// - Did Joffrey agree?
+// - He did. He also said "I love using \n".
+// Note: Use only one console.log()
+
+console.log('- Did Joffrey agree?\n- He did. He also said "I love using \\n".');
