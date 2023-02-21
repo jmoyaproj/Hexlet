65c39b586aa552fa45b5d336b15daf2d3defee4d
jmoyaproj <jmoyaproj@gmail.com>
2023-02-20 22:59:01 -0600

    Completed Defining with Expressions.

---
+++ Exercises/06-Defining-with-Expressions.js
@@ -0,0 +1,9 @@
+// Write a program that takes the initial amount of euros stored in the variable eurosCount,
+// converts euros to dollars, and prints it. Then, it should conver the result
+// to rubles and print it on a new line.
+// Note: 1 euro = 1.25 dollars and 1 dollar = 60 rubles
+
+const eurosCount = 100;
+
+console.log(eurosCount * 1.25);
+console.log(eurosCount * 1.25 * 60);
